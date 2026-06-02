ca680b264d2561174e88401583ae7c118753fcda
NicolaRebola <nicorebo1999@gmail.com>
2026-06-02 17:38:22 -0300

    fix CORS error in

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://life-tracker-894343441345.us-central1.run.app',
+      'https://life-tracker--portfolio-blog-ee307.us-east4.hosted.app',
     ],
     credentials: true,
   });
